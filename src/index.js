3910acaf348115c1823e44592dc8b98d7a62c146
pilium <igorpilium@gmail.com>
2017-12-09 13:29:58 +0300

    add canvas

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Enemy from './pers/enemy'
+import powerdot from './pers/powerdot'
+import player from './pers/player'
+import render from './libs/render'
+
+const canvas = document.createElement('canvas')
+const ctx = canvas.getContext('2d')
+
+const enemy = Enemy()
+
+let data = {
+    pscore: 0,
+    gscore: 0
+  }
+  
+const resizeCanvas = () => {
+    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+    data = render(ctx, canvas, player, enemy, powerdot, data.pscore, data.gscore, image)
+    requestAnimationFrame(resizeCanvas)
+}
+  
+  document.body.appendChild(canvas)
